89f1461775140ded90752006b0b8e50adaaef25f
WBW <wbw@WBWdeMacBook-Pro.local>
2019-10-28 17:16:30 +0800

    getClusterNodes 但不确定是否还是用原来的cluster

--- src/main/scala/testJedis.scala
+++ src/main/scala/testJedis.scala
@@ -1,5 +1,6 @@
-import redis.clients.jedis.{HostAndPort, JedisCluster, ScanParams}
+import redis.clients.jedis.{HostAndPort, JedisCluster, JedisPool, ScanParams}
 import scala.collection.JavaConverters._
+
 /**
  *
  */
@@ -11,13 +12,26 @@ object testJedis {
     val keyOrPattern = args(2)
     val hgetAllKey = args(3)
 
-    println("===== hostStr, keyOrPattern, hgetAllKey =====")
+    println("\n===== hostStr, keyOrPattern, hgetAllKey =====")
     println(hostStr)
     println(keyOrPattern)
     println(hgetAllKey)
 
-    val host = RedisHostParser(hostStr)
-    withCluster(host, {cli =>
+    var nodes: scala.collection.mutable.Map[String, JedisPool] = scala.collection.mutable.Map()
+    val cliHost = RedisHostParser(hostStr)
+    withCluster(Set(cliHost), { cli =>
+      nodes = cli.getClusterNodes.asScala
+    })
+    println("===== cluster nodes =====")
+    println(nodes)
+
+    val HostAndPortSet: Set[RedisHostParser] = nodes.keys.toSet.map {
+      key: String =>
+        val addr = "c@" + key.toString + "/0"
+        RedisHostParser(addr)
+    }
+
+    withCluster(HostAndPortSet, { cli =>
       val scan = new ScanParams().`match`(keyOrPattern).count(50000)
       var result = cli.scan(ScanParams.SCAN_POINTER_START, scan)
       var keys = result.getResult.asScala.toList
@@ -31,14 +45,15 @@ object testJedis {
       val hgetResult = cli.hgetAll(hgetAllKey)
       println("===== hget result =====")
       println(hgetResult)
+      println("")
     })
   }
 
-  def withCluster[T](addr: RedisHostParser, body: JedisCluster => T): T = {
+  def withCluster[T](addrSet: Set[RedisHostParser], body: JedisCluster => T): T = {
     if (cluster == null) {
       synchronized {
         if (cluster == null) {
-          cluster = new JedisCluster(addr.clusterNode)
+          cluster = new JedisCluster(addrSet.map(addr => addr.clusterNode).asJava)
         }
       }
     }
